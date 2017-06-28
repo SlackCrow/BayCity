3f91147964ffb81cad3c90945e91a0b70fc879a3
Slack Crow <slackcrow@users.noreply.github.com>
2017-06-28 12:49:55 +0900

    work in progress uploaded to share with other members

---
+++ app/src/main/java/socalledengineers/baycity/DangeruGetter.java
@@ -0,0 +1,52 @@
+package socalledengineers.baycity;
+
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
+
+/**
+ * Created by Slack Crow on 6/27/2017.
+ */
+
+public class DangeruGetter {
+    public ArrayList<String> getThreadList(String board, Integer limit) throws IOException, org.json.JSONException
+    {
+        String address = "https:////boards.dangeru.us/api.php?type=index&board="+board+"&ln="+limit.toString();
+        // reference https://stackoverflow.com/questions/34691175/how-to-send-httprequest-and-get-json-response-in-android
+
+        HttpURLConnection urlConnection = null;
+        URL url = new URL(address);
+        urlConnection = (HttpURLConnection) url.openConnection();
+
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(10000);
+        urlConnection.setConnectTimeout(15000);
+
+        urlConnection.setDoOutput(true);
+
+        urlConnection.connect();
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
+
+        char[] buffer = new char[1024];
+
+        String jsonString = new String();
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line+"\n");
+        }
+        br.close();
+
+        // reference ends
+
+        JSONObject jsonObj = new JSONObject(sb.toString());
+        JSONArray jsonArr = jsonObj.getJSONArray("meta");
+        return null;
+    }
+}
--- app/src/main/java/socalledengineers/baycity/MainActivity.java
+++ app/src/main/java/socalledengineers/baycity/MainActivity.java
@@ -2,12 +2,35 @@ package socalledengineers.baycity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    DangeruGetter _apiWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _apiWrapper = new DangeruGetter();
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v){
+            EditText boardName = (EditText) findViewById(R.id.editText);
+            String boardNameString = boardName.getText().toString();
+            try {
+                _apiWrapper.getThreadList(boardNameString, 10);
+            }
+            catch (IOException e){
+                System.out.println("Fetch Error");
+            }
+            catch(org.json.JSONException e){
+                System.out.println("JSON Error");
+            }
+        }
+        });
     }
 }
